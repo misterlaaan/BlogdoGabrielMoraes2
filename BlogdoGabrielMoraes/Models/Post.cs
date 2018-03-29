c63f8b9691b199c46a401418aa49c73b8f121b87
GABRIEL LOURENCO DE MORAES <gabriel.lmoraes@senacsp.edu.br>
2018-03-29 09:00:40 -0300

    Adicionar arquivos de projeto.

---
+++ BlogdoGabrielMoraes/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogdoGabrielMoraes.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(500)]
+        public string Title { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateUpdated { get; set; }
+        [Required]
+        [StringLength(8000)]
+        public string PostingBody { get; set; }
+    }
+}
---
+++ BlogdoGabrielMoraes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogdoGabrielMoraes.Startup))]
+namespace BlogdoGabrielMoraes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
